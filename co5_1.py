a9474c7eda9f866ebf4a6f9d15d49fd5a8ab1afd
Anagha-K2004 <kanagha2004@gmail.com>
2025-10-16 23:56:40 -0700

    Add files via upload

---
+++ co5_1.py
@@ -0,0 +1,10 @@
+#write a program to read a file line by line and store it into a list
+file=open("demo.txt","r")
+content=file.readlines()
+linelist=[]
+for i in content:
+    print(i)
+    line=i.strip()
+    linelist.append(line)
+print(linelist)
+file.close()
