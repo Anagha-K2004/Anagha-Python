427d284491c09b00c4fbc36bafd16bf2b7c99dc8
Anagha-K2004 <kanagha2004@gmail.com>
2025-10-24 11:37:30 +0530

    Add files via upload

---
+++ co2_6.py
@@ -0,0 +1,12 @@
+#Total number of characters in a string
+str=input("Enter the string:")
+dict={}
+for i in str:
+    if i in dict:
+        dict[i]+=1
+    else:
+        dict[i]=1
+for k,v in dict.items():
+    print(k,"=",v)
+        
+        
