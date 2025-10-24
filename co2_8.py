14bcbf472364914c91b39866eff2a1707ebf22ea
Anagha-K2004 <kanagha2004@gmail.com>
2025-10-24 12:18:05 +0530

    Add files via upload

---
+++ co2_8.py
@@ -0,0 +1,13 @@
+#co2_8Accept a list of words and return the length of the longest word
+l=[]
+limit=int(input("Enter the limit:"))
+for i in range(limit):
+    word=input("Enter the word:")
+    l.append(word)
+print(l)
+le=0    
+for i in l:
+    if len(i)>le:
+        le=len(i)
+        longword=i
+print("Length of the longest word is:",le,longword)
