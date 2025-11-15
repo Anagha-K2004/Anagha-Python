39b8761bd7ce3e828ef4cf6ffdf5d47ba8458eee
Anagha-K2004 <kanagha2004@gmail.com>
2025-11-15 15:13:20 +0530

    Add files via upload

---
+++ co1_15.py
@@ -0,0 +1,16 @@
+list1=[]
+limit=int(input("Enter the limit:"))
+for i in range(limit):
+    element=input("Enter the color:")
+    list1.append(element)
+print(list1)
+list2=[]
+limit=int(input("Enter the limit:"))
+for i in range(limit):
+    element2=input("Enter the color:")
+    list2.append(element2)
+print(list2)
+for i in list1:
+    if i not in list2:
+        print(i)
+        
